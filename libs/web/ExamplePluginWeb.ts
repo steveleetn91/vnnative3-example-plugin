1743478dbbae6be879d6740498a112c7226be4db
CEO <hoang.le.tn91@gmail.com>
2022-03-01 15:20:06 +0700

    Update core

--- libs/web/ExamplePluginWeb.ts
+++ libs/web/ExamplePluginWeb.ts
@@ -3,7 +3,7 @@ import ExamplePluginWebInterface from "./ExamplePluginWeb.interface";
 export default class ExamplePluginWeb implements ExamplePluginWebInterface {
     getData() : string {
         let win : any = window;
-        let data :string = ""
+        let data :string = "Web - example plugin"
         if(win.exampleplugin) {
             data = win.exampleplugin.getData() ? win.exampleplugin.getData() : "";
         }
